91d92586e7a98daffed64038764f594bdd008e2e
Remotas <remotas.work@gmail.com>
2025-05-01 14:38:48 -0400

    Componentes Padres, Componentes Hijos, Props y Propiedades Computadas

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,7 +1,10 @@
 <template>
+  <Header :mensaje="texto"></Header>
   <div class="container">
     <sidebar></sidebar>
     <div class="content">
+      <input type="texto" v-model="texto" />
+      {{ texto }}
       <router-view></router-view>
     </div>
   </div>
@@ -9,6 +12,9 @@
 
 <script setup>
 import sidebar from '../components/Sidebar.vue'
+import Header from '../components/Header.vue'
+import { ref } from 'vue'
+const texto = ref('')
 </script>
 
 <style>
